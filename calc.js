ead80d77ebf280a76ca9afa337d597a0364b2bb5
duh <62608915+duh@users.noreply.github.com>
2024-04-29 15:33:44 +0100

    Basic structure

---
+++ calc.js
@@ -0,0 +1,10 @@
+// Level calculator for scratch.mit.edu
+// Inspired by clockwork's ROBLOX level calculator
+// Please credit if using script @duh/@icicubez
+
+const calculateButton = document.getElementById("calculateButton");
+const username = document.getElementById("username").textContent;
+calculateButton.addEventListener("click", (username) =>
+{
+
+});
